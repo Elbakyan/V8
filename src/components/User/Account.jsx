855620e7b16efe3d385e6961ea1b6716f7895b2d
Noro <norojan56@mail.ru>
2020-09-19 16:11:19 +0400

    account

--- src/components/User/Account.jsx
+++ src/components/User/Account.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Header from "./Header";
 import {Route} from "react-router";
 import SpaerParts from '../spare/SpareParts'
+import Footer from "../Footer/Footer";
 class Account extends React.Component{
     render() {
         return (
             <div className="Account">
                 <Header/>
+                <Route exact path='/user/account'>
+                    <div className="Account__container">
+
+                    </div>
+                </Route>
                 <Route exact path='/user/account/spare-parts'>
                     <SpaerParts/>
                 </Route>
+                <Footer/>
             </div>
         );
     }
