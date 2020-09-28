22eaf161e355d36fe5d54b9dc63d6207cc3bde21
Noro <norojan56@mail.ru>
2020-09-28 10:34:23 +0400

    cars

--- src/redux/auto/AutoReducer.js
+++ src/redux/auto/AutoReducer.js
@@ -1,42 +1,22 @@
 import {GET_MODEL, GET_MARK} from "./action";
 
-import {body, category, color, royle, transmission} from "./Values";
+import {category, color, engine, fuel, royle, traction, transmission, type, year} from "./Values";
 
 
 const initalState = {
     mark: [],
     model: [],
-    year:(()=>{
-        let temp = [];
-        let year = new Date().getFullYear();
-        for (let i = year; i >= 1975; i--){
-            temp.push(i);
-        }
-        return temp;
-    })(),
-    color:color,
-    body:body,
     royle:royle,
-    category:category,
+    color:color,
+    year: year,
+    fuel: fuel,
     transmission:transmission,
+    engine: engine,
+    type: type,
+    category:category,
+    traction: traction
 
-    engine:(() => {
-        let temp = [];
-        for (let i = 0; i < 7; i++){
-            if(i > 0 ){
-                temp.push(i + '.0')
-            }
-            for (let j = 1; j < 10; j++){
-                if(i == 0 && j >= 8){
-                    temp.push(i + '.' + j)
-                }else if(i > 0){
-                    temp.push(i + '.' + j)
-                }
-            }
-        }
-        temp.push("7.0")
-        return temp;
-    })(),
+
 }
 
 
