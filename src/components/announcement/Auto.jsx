f4ec99a7238c77187dfb0809fbecf3bd5aca7206
Elbakyan <elbakyanit@gmail.com>
2020-09-30 17:52:22 +0400

    'carinfo'

--- src/components/announcement/Auto.jsx
+++ src/components/announcement/Auto.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDollarSign} from '@fortawesome/free-solid-svg-icons'
+import { faMobileAlt} from '@fortawesome/free-solid-svg-icons'
 import DefaultInput from "../forms/inputs/DefaultInput";
 import DefaultBtn from "../forms/buttons/DefaultBtn";
 import DefaultSelect from "../forms/select/DefaultSelect";
@@ -27,6 +27,7 @@ class Auto extends Component {
             year,
             fuel,
             engine,
+            phone,
             number,
             vin,
             img,
@@ -42,75 +43,79 @@ class Auto extends Component {
         } = this.props.data
         return (
             <div className="Auto">
-                <div className='block-left'>
-                    <div className="car_name">
-                        <span>{model}</span>
+                <div className="car_name">
+                    <span>{model}</span>
+                    <span className="phone_car_owner"><FontAwesomeIcon icon={faMobileAlt} />{phone}</span>
+                </div>
+                <div className="block">
+                    <div className='block-left'>
+                        <div className="car_slider">
+                            <SliderAuto autoImage={img}/>
+                        </div>
                     </div>
-                    <div className="car_slider">
-                        <SliderAuto autoImage={img}/>
+
+                    <div className='block-right'>
+                        <div className="car_info">
+                            <table className="auto_parapters">
+                                <tbody>
+                                <tr>
+                                    <td>Շարժիչը</td>
+                                    <td>{engine}</td>
+                                </tr>
+                                <tr>
+                                    <td>Ձիաուժը</td>
+                                    <td>{power}</td>
+                                </tr>
+                                <tr>
+                                    <td>Տարեթիվ</td>
+                                    <td>{year}</td>
+                                </tr>
+                                <tr>
+                                    <td>Գույն</td>
+                                    <td>{color}</td>
+                                </tr>
+                                <tr>
+                                    <td>Փոխանցման տուփը</td>
+                                    <td>{transmission_box}</td>
+                                </tr>
+                                <tr>
+                                    <td>Ղեկը</td>
+                                    <td>{royle}</td>
+                                </tr>
+                                <tr>
+                                    <td>Քարշակը</td>
+                                    <td>{traction}</td>
+                                </tr>
+                                <tr>
+                                    <td>Նույնացման համար (VIN)</td>
+                                    <td>{vin}</td>
+                                </tr>
+                                <tr>
+                                    <td>Վառելիքը</td>
+                                    <td>{fuel}</td>
+                                </tr>
+                                <tr>
+                                    <td>Թափքի տեսակը</td>
+                                    <td>{type}</td>
+                                </tr>
+                                <tr>
+                                    <td>ՏՄ Տեսակը</td>
+                                    <td>{category}</td>
+                                </tr>
+                                <tr>
+                                    <td>Վազք</td>
+                                    <td>{mileage} {mesuruments}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div className="description_text">
+                            <p>{desc}</p>
+                        </div>
                     </div>
                 </div>
 
-                <div className='block-right'>
-                    <div className="car_info">
-                        <table className="auto_parapters">
-                            <tbody>
-                            <tr>
-                                <td>Շարժիչը</td>
-                                <td>{engine}</td>
-                            </tr>
-                            <tr>
-                                <td>Ձիաուժը</td>
-                                <td>{power}</td>
-                            </tr>
-                            <tr>
-                                <td>Տարեթիվ</td>
-                                <td>{year}</td>
-                            </tr>
-                            <tr>
-                                <td>Գույն</td>
-                                <td>{color}</td>
-                            </tr>
-                            <tr>
-                                <td>Փոխանցման տուփը</td>
-                                <td>{transmission_box}</td>
-                            </tr>
-                            <tr>
-                                <td>Ղեկը</td>
-                                <td>{royle}</td>
-                            </tr>
-                            <tr>
-                                <td>Քարշակը</td>
-                                <td>{traction}</td>
-                            </tr>
-                            <tr>
-                                <td>Նույնացման համար (VIN)</td>
-                                <td>{vin}</td>
-                            </tr>
-                            <tr>
-                                <td>Վառելիքը</td>
-                                <td>{fuel}</td>
-                            </tr>
-                            <tr>
-                                <td>Թափքի տեսակը</td>
-                                <td>{type}</td>
-                            </tr>
-                            <tr>
-                                <td>ՏՄ Տեսակը</td>
-                                <td>{category}</td>
-                            </tr>
-                            <tr>
-                                <td>Վազք</td>
-                                <td>{mileage} {mesuruments}</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div className="description_text">
-                        <p>{desc}</p>
-                    </div>
-                </div>
             </div>
         )
     }
