a83bf4e58a046956e2ee65cabe7ef28b962d4a9e
Elbakyan <elbakyanit@gmail.com>
2020-10-08 17:51:28 +0400

    'carrrr'

--- src/components/User/UserCars/UserCars.jsx
+++ src/components/User/UserCars/UserCars.jsx
@@ -15,99 +15,7 @@ import {POST, TEST_POST} from "../../config/Requsest";
 import {Url} from "../../config/Url";
 import {connect} from "react-redux";
 const status = 0
-const usAuto = [
-    {
-        id: 1,
-        user_id: 11,
-        mark: 'BMW',
-        model: 'BMW M5',
-        royle: 'Ձախ',
-        transmission_box: 'Ավտոմատ',
-        color: '-',
-        year: '2015',
-        fuel: 'Բենզին',
-        transmission: '',
-        engine: '5,0',
-        number: '99 VA 969',
-        vin: '740-VH4477',
-        img: [
-            'https://ag-spots-2015.o.auroraobjects.eu/2015/02/14/other/2880-1800-crop-bmw-m5-f10-30-jahre-edition-c160914022015234303_1.jpg',
-            'https://bmw.vin.ua/files/images/units/covers/big/news-1005181.jpg',
-            'https://www.ludoviccareme.com/files/image_211_image_fr.jpg',
-            'https://usetrans.com/wp-content/uploads/2018/05/BMW-M5-Competition-2018-2019-7-min.jpg',
-            'https://i.pinimg.com/originals/d5/90/1b/d5901b60b9fd42ec907d79d473ea5bd0.jpg'
-        ],
-        type: 'Սեդան',
-        category: 'Թեթև մարդատար',
-        traction: '4 քարծշակ',
-        power: '401',
-        price: '',
-        milage: '',
-        desc: '',
-        state: '',
-        data: ''
-    },
-    {
-        id: 2,
-        user_id: 11,
-        mark: 'BMW',
-        model: 'BMW M5',
-        royle: 'Ձախ',
-        transmission_box: 'Ավտոմատ',
-        color: 'Կարմիր',
-        year: '2015',
-        fuel: 'Բենզին',
-        transmission: '',
-        engine: '5,0',
-        number: 'XX YY XXX',
-        vin: '740-VH4477',
-        img: [
-            'https://ag-spots-2015.o.auroraobjects.eu/2015/02/14/other/2880-1800-crop-bmw-m5-f10-30-jahre-edition-c160914022015234303_1.jpg',
-            'https://bmw.vin.ua/files/images/units/covers/big/news-1005181.jpg',
-            'https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/images/Res_the-new-bmw-m5-compe890_3.jpg',
-            'https://usetrans.com/wp-content/uploads/2018/05/BMW-M5-Competition-2018-2019-7-min.jpg',
-            'https://usetrans.com/wp-content/uploads/2018/05/BMW-M5-Competition-2018-2019-7-min.jpg',
-            'https://usetrans.com/wp-content/uploads/2018/05/BMW-M5-Competition-2018-2019-7-min.jpg',
-            'https://ag-spots-2015.o.auroraobjects.eu/2015/02/14/other/2880-1800-crop-bmw-m5-f10-30-jahre-edition-c160914022015234303_1.jpg',
-        ],
-        type: 'Սեդան',
-        category: 'Թեթև մարդատար',
-        traction: '4 քարծշակ',
-        power: '401',
-        price: '',
-        milage: '',
-        desc: '',
-        state: '',
-        data: ''
-    },
-    {
-        id: 3,
-        user_id: 11,
-        mark: 'BMW',
-        model: 'BMW M5',
-        royle: 'Ձախ',
-        transmission_box: 'Ավտոմատ',
-        color: 'Կարմիր',
-        year: '2015',
-        fuel: 'Բենզին',
-        transmission: '',
-        engine: '5,0',
-        number: '77 VS 777',
-        vin: '740-VH4477',
-        img: [
-            'https://ag-spots-2015.o.auroraobjects.eu/2015/02/14/other/2880-1800-crop-bmw-m5-f10-30-jahre-edition-c160914022015234303_1.jpg',
-        ],
-        type: 'Սեդան',
-        category: 'Թեթև մարդատար',
-        traction: '4 քարծշակ',
-        power: '401',
-        price: '',
-        milage: '',
-        desc: '',
-        state: '',
-        data: ''
-    },
-]
+
 
 class UserCars extends Component {
     constructor(props) {
@@ -196,7 +104,7 @@ class UserCars extends Component {
         })
     }
     render() {
-
+        console.log(this.props.auto.auto)
         return (
             <div>
                 {/*{this.state.Redirect ? <Redirect to='/user/account/cars' /> : ''}*/}
