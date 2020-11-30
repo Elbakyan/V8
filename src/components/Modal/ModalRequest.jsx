1b0b039c9437f1ab03d9e3e22a67e206cfb1162a
Elbakyan <elbakyanit@gmail.com>
2020-11-30 11:11:31 +0400

    'model'

--- src/components/Modal/ModalRequest.jsx
+++ src/components/Modal/ModalRequest.jsx
@@ -6,6 +6,9 @@ import {connect} from "react-redux";
 import {maser} from '../Menu/autoObj'
 import DefaultSelect from "../forms/select/DefaultSelect";
 import DefaultInput from "../forms/inputs/DefaultInput";
+import SpeareParts from "./SpeareParts";
+import Test from "./Test";
+import {GetModel} from "../../redux/auto/action";
 
 class ModalRequest extends React.Component{
     constructor(props) {
@@ -60,124 +63,134 @@ class ModalRequest extends React.Component{
                         <div className='modal__items'>
                             <DefaultSelect name='' data={maser} />
                         </div>
-                        <div className='modal__items'>
-                            <textarea name="" placeholder='Նկարագրությունը․․․'></textarea>
-                        </div>
-                        <div className='modal__items'>
-                            <DefaultSelect name='sircle' data={this.props.location.sircle} />
-                        </div>
-                        <div className='modal__items'>
-                            <select onChange={this.MyCar}>
-                                <option value=''>Մեքենա</option>
-                                {
-                                    this.props.auto.auto.data? this.props.auto.auto.data.map(res => {
-                                        return (
-                                            <option
-                                                value={res.mark}
-                                                data-model={res.model}
-                                                data-vin={res.vin}
-                                                data-year={res.year}
-                                                data-engine={res.engine}
-                                            >{res.model}</option>
-                                        )
-                                    }):''
-                                }
-                            </select>
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    <textarea name="" placeholder='Նկարագրությունը․․․'></textarea>*/}
+                        {/*</div>*/}
+                        {/*<div className='modal__items'>*/}
+                        {/*    <DefaultSelect name='sircle' data={this.props.location.sircle} />*/}
+                        {/*</div>*/}
+                        {/*<div className='modal__items'>*/}
+                        {/*    <select onChange={this.MyCar}>*/}
+                        {/*        <option value=''>Մեքենա</option>*/}
+                        {/*        {*/}
+                        {/*            this.props.auto.auto.data? this.props.auto.auto.data.map(res => {*/}
+                        {/*                return (*/}
+                        {/*                    <option*/}
+                        {/*                        value={res.mark}*/}
+                        {/*                        data-model={res.model}*/}
+                        {/*                        data-vin={res.vin}*/}
+                        {/*                        data-year={res.year}*/}
+                        {/*                        data-engine={res.engine}*/}
+                        {/*                    >{res.model}</option>*/}
+                        {/*                )*/}
+                        {/*            }):''*/}
+                        {/*        }*/}
+                        {/*    </select>*/}
+                        {/*</div>*/}
 
-                        <div className='modal__items'>
-                            <select name="year">
-                                {
-                                    this.state.year?
-                                        this.state.year.map(res => {
-                                            return (
-                                                <option key={res} value={res}>{res}</option>
-                                            )
-                                        }):
-                                        this.props.auto.year.map(res => {
-                                            return (
-                                                <option key={res}>{res}</option>
-                                            )
-                                        })
-                                }
-                            </select>
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    <select name="year">*/}
+                        {/*        {*/}
+                        {/*            this.state.year?*/}
+                        {/*                this.state.year.map(res => {*/}
+                        {/*                    return (*/}
+                        {/*                        <option key={res} value={res}>{res}</option>*/}
+                        {/*                    )*/}
+                        {/*                }):*/}
+                        {/*                this.props.auto.year.map(res => {*/}
+                        {/*                    return (*/}
+                        {/*                        <option key={res}>{res}</option>*/}
+                        {/*                    )*/}
+                        {/*                })*/}
+                        {/*        }*/}
+                        {/*    </select>*/}
+                        {/*</div>*/}
 
-                        <div className='modal__items'>
-                            {
-                                this.state.mark?
-                                    <select name="mark">
-                                        <option value={this.state.mark}>
-                                            {this.state.mark}
-                                        </option>
-                                    </select>:
-                                    <DefaultSelect name='mark' data={this.props.auto.mark} />
-                            }
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    {*/}
+                        {/*        this.state.mark?*/}
+                        {/*            <select name="mark">*/}
+                        {/*                <option value={this.state.mark}>*/}
+                        {/*                    {this.state.mark}*/}
+                        {/*                </option>*/}
+                        {/*            </select>:*/}
+                        {/*            <DefaultSelect name='mark' data={this.props.auto.mark} />*/}
+                        {/*    }*/}
+                        {/*</div>*/}
 
-                        <div className='modal__items'>
-                            <select name="engine">
-                                {
-                                    this.state.engine?
-                                        this.state.engine.map(res => {
-                                            return (
-                                                <option key={res} value={res}>{res}</option>
-                                            )
-                                        }):
-                                        this.props.auto.engine.map(res => {
-                                            return (
-                                                <option key={res}>{res}</option>
-                                            )
-                                        })
-                                }
-                            </select>
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    <select name="engine">*/}
+                        {/*        {*/}
+                        {/*            this.state.engine?*/}
+                        {/*                this.state.engine.map(res => {*/}
+                        {/*                    return (*/}
+                        {/*                        <option key={res} value={res}>{res}</option>*/}
+                        {/*                    )*/}
+                        {/*                }):*/}
+                        {/*                this.props.auto.engine.map(res => {*/}
+                        {/*                    return (*/}
+                        {/*                        <option key={res}>{res}</option>*/}
+                        {/*                    )*/}
+                        {/*                })*/}
+                        {/*        }*/}
+                        {/*    </select>*/}
+                        {/*</div>*/}
 
-                        <div className='modal__items'>
-                            {
-                                this.state.model?
-                                    <select name="model">
-                                        <option value={this.state.model}>
-                                            {this.state.model}
-                                        </option>
-                                    </select>:
-                                    <DefaultSelect name='model' data={this.props.auto.model} />
-                            }
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    {*/}
+                        {/*        this.state.model?*/}
+                        {/*            <select name="model">*/}
+                        {/*                <option value={this.state.model}>*/}
+                        {/*                    {this.state.model}*/}
+                        {/*                </option>*/}
+                        {/*            </select>:*/}
+                        {/*            <DefaultSelect name='model' data={this.props.auto.model} />*/}
+                        {/*    }*/}
+                        {/*</div>*/}
 
-                        <div className='modal__items'>
-                            <input
-                                name='vin'
-                                defaultValue={this.state.vin?this.state.vin:''}
-                                placeholder='VIN'
-                            />
-                        </div>
-                        <div className="modal__items">
-                                <input name='code' type="text" placeholder='Դետալի կոդը'/>
-                        </div>
-                        <div className='modal__file'>
-                            <label className='modal__file-label'>
-                                Ներբեռնել նկար
-                                <input name='img' type="file"/>
-                            </label>
-                        </div>
-                        <div className="modal__items">
-                            <label>
-                                Նոր
-                                <input defaultChecked name='new' value={1} type="checkbox"/>
-                            </label>
-                        </div>
-                        <div className="modal__items">
-                            <label>
-                                Օգտ․
-                                <input defaultChecked name='old' value={1} type="checkbox"/>
-                            </label>
-                        </div>
-                        <div className="modal__items">
-                            <button className='modal__button'>
-                                Ուղարկել
-                            </button>
-                        </div>
+                        {/*<div className='modal__items'>*/}
+                        {/*    <input*/}
+                        {/*        name='vin'*/}
+                        {/*        defaultValue={this.state.vin?this.state.vin:''}*/}
+                        {/*        placeholder='VIN'*/}
+                        {/*    />*/}
+                        {/*</div>*/}
+                        {/*<div className="modal__items">*/}
+                        {/*        <input name='code' type="text" placeholder='Դետալի կոդը'/>*/}
+                        {/*</div>*/}
+                        {/*<div className='modal__file'>*/}
+                        {/*    <label className='modal__file-label'>*/}
+                        {/*        Ներբեռնել նկար*/}
+                        {/*        <input name='img' type="file"/>*/}
+                        {/*    </label>*/}
+                        {/*</div>*/}
+                        {/*<div className="modal__items">*/}
+                        {/*    <label>*/}
+                        {/*        Նոր*/}
+                        {/*        <input defaultChecked name='new' value={1} type="checkbox"/>*/}
+                        {/*    </label>*/}
+                        {/*</div>*/}
+                        {/*<div className="modal__items">*/}
+                        {/*    <label>*/}
+                        {/*        Օգտ․*/}
+                        {/*        <input defaultChecked name='old' value={1} type="checkbox"/>*/}
+                        {/*    </label>*/}
+                        {/*</div>*/}
+                        {/*<div className="modal__items">*/}
+                        {/*    <button className='modal__button'>*/}
+                        {/*        Ուղարկել*/}
+                        {/*    </button>*/}
+                        {/*</div>*/}
+                        <SpeareParts
+                            sircle={this.props.location.sircle}
+                            auto={this.props.auto.auto.data}
+                            year={this.props.auto.year}
+                            mark={this.props.auto.mark}
+                            engine={this.props.auto.engine}
+                            model={this.props.auto.model}
+                            dis={this.props.dispatch}
+                        />
+                        {/*<Test />*/}
                     </div>
                 </div>
             </form>
