cb01879bea11546319512426c51c8110d04b4424
Elbakyan <elbakyanit@gmail.com>
2020-12-01 15:15:51 +0400

    'ssss'

--- src/components/GetRequest/GetRequest.jsx
+++ src/components/GetRequest/GetRequest.jsx
@@ -1,104 +1,31 @@
 import React, {Component, Fragment} from "react";
-import '../User/message/Message.scss';
+// import '../User/message/Message.scss';
 import './GetRequest.scss';
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {Route} from "react-router";
 import DefaultBtn from "../forms/buttons/DefaultBtn";
-import {POST} from "../config/Requsest";
-import {Url} from "../config/Url";
-import {GetRequst} from "../../redux/GetRequest/action";
-import {GetStatus, SendMessage} from "../../redux/message/action";
 
 
 
 class GetRequest extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: '',
-            dialog: '',
-            scroll: true,
-            cou:0
-        }
-        this.textareaRef = React.createRef()
-        this.formRef = React.createRef()
-        this.scrollRef = React.createRef()
-    }
-    componentDidMount() {
-        let id = window.location.pathname.split('/').pop();
-        let dialog = window.location.pathname.split('/')[window.location.pathname.split('/').length - 2];
-        this.setState({
-            id: id,
-            dialog: dialog
-        })
-        setTimeout(() => {
-            // this.scroll();
-        },1000)
-
-    }
-
-    Send = (e) => {
-        this.setState({
-            id: e.target.dataset.id,
-            dialog: e.target.dataset.dialog
-        })
-    }
-    Message = (e) => {
-        e.preventDefault();
-        let data = new FormData(e.target);
-        POST(Url.sendrequest,data).then(res => {
-            this.props.dispatch(GetRequst())
-            this.textareaRef.current.value = ''
-        })
-        this.scroll();
-
-    }
-    scroll = () => {
-        this.scrollRef.current.scrollTop = this.scrollRef.current.scrollHeight;
-        if(this.state.cou != 2){
-            setTimeout(()=>{
-                this.setState({
-                    cou:++this.state.cou
-                })
-                this.scroll()
-            },40)
-        }else{
-            this.setState({
-                cou:0
-            })
-        }
-    }
-    onEnterPress = (e) => {
-        if(e.keyCode == 13 && e.shiftKey == false) {
-            e.preventDefault();
-            let data = new FormData(this.formRef.current);
-            if (this.textareaRef.current.value.trim().length > 0){
-                POST(Url.sendrequest,data).then(res => {
-                    this.props.dispatch(GetRequst())
-                    this.textareaRef.current.value = '';
-                    this.scroll()
-                })
-            }
-
-        }
-
-    }
     render() {
+        console.log(this.props.request.request)
         return(
             <div className='message_users_component'>
                 <div className="message_users">
                     <div className="respondent">
-                        <ul className='users' >
+                        <ul className='users'>
                             {
                                 this.props.request.request.map((el,i)=>{
+                                    console.log(el)
                                     return(
-                                        <Link key={i} onClick={this.Send}
-                                              to={this.props.score.score.status? '/score/account/request/'+el.message[0].dialog + '/' + el.user.id
-                                                  :'user/account/request/'+el.message[0].dialog + '/' + el.user.id}
-
-                                        >
-                                            <ul className={'R__link'} data-id={el.user.id} data-dialog={el.message[0].dialog}>
+                                        <Link key={i}
+                                              to={this.props.score.score.status? '/score/account/request/'+el.message[0].dialog
+                                                  :
+                                                  'user/account/request/'+el.message.dialog
+                                              }>
+                                            <ul >
                                                 <li>{el.user.name}{el.user.surname}</li>
                                                 <li><img src={el.user.img} alt=""/></li>
                                             </ul>
@@ -109,26 +36,27 @@ class GetRequest extends Component{
                         </ul>
 
                     </div>
-                    <div className="get_message" >
+                    <div className="get_message">
                         {
                             this.props.request.request.map((el,i)=>{
                                 return(
-                                    <Route key={i} path={this.props.score.score.status? '/score/account/request/'+el.message[0].dialog + '/' + el.user.id
+                                    <Route key={i} path={this.props.score.score.status? '/score/account/request/'+el.message[0].dialog
                                         :
-                                        'user/account/request/'+el.message.dialog + '/' + el.user.id
+                                        'user/account/request/'+el.message.dialog
                                     }>
-                                        <div className="message" >
-                                            <ul ref={this.scrollRef}>
-                                                <li className='get_message' >
-                                                    <ul className='message_request_style' >
+                                        <div className="message">
+                                            <ul>
+                                                <li className='msStyle get'>
+                                                    <ul className='message_request_style'>
                                                         <li>1111</li>
                                                     </ul>
                                                 </li>
                                                 {
                                                     el.message.map((mess,i)=>{
+                                                        console.log(mess)
                                                         return(
-                                                            <li className={'message_request ' + this.props.score.score.id || this.props.user.id === mess.send?'send_message':'get_message'}>
-                                                                <ul className='message_request_style' >
+                                                            <li className={this.props.score.score.id || this.props.user.id === mess.send?'msStyle send':'msStyle get'}>
+                                                                <ul className='message_request_style'>
                                                                     {
                                                                         mess.img ||  mess.message.mark ||
                                                                         mess.message.model || mess.message.engine ||
@@ -143,16 +71,16 @@ class GetRequest extends Component{
 
                                                                             <div className='message_info__description'>
                                                                                 {
-                                                                                    mess.message.mark? <li>{mess.message.mark}</li> :''
+                                                                                    mess.message.mark? <li>Մակնիշ - {mess.message.mark}</li> :''
                                                                                 }
                                                                                 {
-                                                                                    mess.message.model? <li>{mess.message.model}</li> :''
+                                                                                    mess.message.model? <li>Մոդել - {mess.message.model}</li> :''
                                                                                 }
                                                                                 {
-                                                                                    mess.message.engine? <li>{mess.message.engine}</li> :''
+                                                                                    mess.message.engine? <li>Շարժիճի ծավալը ։{mess.message.engine}</li> :''
                                                                                 }
                                                                                 {
-                                                                                    mess.message.year? <li>{mess.message.year}</li> :''
+                                                                                    mess.message.year? <li>Տարեթիվ ։{mess.message.year}</li> :''
                                                                                 }
                                                                                 {
                                                                                     mess.message.vin? <li>VIN:({mess.message.vin})</li> :''
@@ -164,7 +92,7 @@ class GetRequest extends Component{
                                                                                     mess.message.old? <li>{'Օգտագործված'}</li> :''
                                                                                 }
                                                                                 {
-                                                                                    mess.message.sircle? <li>{ mess.message.sircle}</li> :''
+                                                                                    mess.message.sircle? <li>Տարածաշրջան ։{ mess.message.sircle}</li> :''
                                                                                 }
                                                                             </div>
 
@@ -173,7 +101,7 @@ class GetRequest extends Component{
 
                                                                     <div className='message_text'>
                                                                         {
-                                                                            mess.message.message? <li>{ mess.message.message}</li> :''
+                                                                            mess.message.message || mess.message.message!== ''? <li>{ mess.message.message}</li> :''
                                                                         }
                                                                     </div>
 
@@ -184,10 +112,10 @@ class GetRequest extends Component{
                                                 }
                                             </ul>
                                             <div className='send_message'>
-                                                <form onSubmit={this.Message} ref={this.formRef}>
-                                                    <textarea className="message_text" onKeyDown={this.onEnterPress} name="message" ref={this.textareaRef}></textarea>
-                                                    <input type="hidden" name='get' value={this.state.id?this.state.id: ''}/>
-                                                    <input type="hidden" name='dialog' value={this.state.dialog?this.state.dialog:''}/>
+                                                <form onSubmit={this.Message} ref={el => this.formRef = el}>
+                                                    <textarea className="message_text" onKeyDown={this.onEnterPress} name="message" ref={el => this.textareaRef = el}></textarea>
+                                                    <input type="hidden" name='get' value={this.props.user.id }/>
+                                                    <input type="hidden" name='get_id' value={this.props.getId}/>
                                                     <div className="message_send_button">
                                                         <DefaultBtn
                                                             type='submit'
@@ -200,6 +128,8 @@ class GetRequest extends Component{
                                                 </form>
                                             </div>
                                         </div>
+
+
 
                                     </Route>
                                 )
